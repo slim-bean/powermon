9f91632a3dca22e7d69391d15ac709545dad6944
Edward Welch <edward.welch@grafana.com>
2022-03-13 11:13:00 -0400

    sending to Loki

--- go.mod
+++ go.mod
@@ -1,10 +1,40 @@
-module powermon
+module github.com/slim-bean/powermon
 
 go 1.17
 
-require go.bug.st/serial v1.3.5
+require (
+	github.com/go-kit/kit v0.10.0
+	github.com/grafana/loki-client-go v0.0.0-20210114103019-6057f29bbf5b
+	github.com/prometheus/common v0.15.0
+	go.bug.st/serial v1.3.5
+)
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/creack/goselect v0.1.2 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.2 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.9.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/prometheus/prometheus v1.8.2-0.20201028100903-3245b3267b24 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	golang.org/x/net v0.0.0-20210917221730-978cfadd31cf // indirect
 	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210917145530-b395a37504d4 // indirect
+	google.golang.org/grpc v1.40.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,57 +2,101 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
-	"log"
-	"powermon/pkg/eg4"
+	"github.com/prometheus/common/model"
+	"os"
+	"strings"
+
 	"time"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/grafana/loki-client-go/loki"
+	"go.bug.st/serial"
+
+	"github.com/slim-bean/powermon/pkg/eg4"
 )
-import "go.bug.st/serial"
+
+var (
+	labels = model.LabelSet{
+		"job":  "powermon",
+		"type": "batt",
+	}
+	labels_raw = model.LabelSet{
+		"job":  "powermon",
+		"type": "batt_raw",
+	}
+)
 
 func main() {
+	var logger log.Logger
+	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = log.With(logger, "ts", log.DefaultTimestamp, "caller", log.DefaultCaller)
+	logger = level.NewFilter(logger, level.AllowDebug())
+
+	cfg := loki.Config{}
+	// Sets defaults as well as anything from the command line
+	cfg.RegisterFlags(flag.CommandLine)
+	flag.Parse()
+
+	c, err := loki.NewWithLogger(cfg, logger)
+	if err != nil {
+		level.Error(logger).Log("msg", "failed to create client", "err", err)
+	}
+
 	mode := &serial.Mode{
 		BaudRate: 9600,
 		DataBits: 8,
 	}
 	port, err := serial.Open("/dev/ttyUSB0", mode)
 	if err != nil {
-		log.Fatal(err)
+		level.Error(logger).Log("msg", "failed to open serial port", "err", err)
+		os.Exit(1)
 	}
 
 	buff := make([]byte, 500)
 	for {
-
+		time.Sleep(1000 * time.Millisecond)
 		n, err := port.Write([]byte{0x7E, 0x01, 0x01, 0x00, 0xFE, 0x0D})
 		if err != nil {
-			log.Fatal(err)
+			level.Error(logger).Log("msg", "failed to send command on serial port", "err", err)
+			continue
 		}
 
 		n, err = port.Read(buff)
 		if err != nil {
-			log.Fatal(err)
-			break
+			level.Error(logger).Log("msg", "failed to read from serial port", "err", err)
+			continue
 		}
 
 		if buff[0] == 0x7E && buff[n-1] == 0x0D {
-			//for i := 0; i < n; i++ {
-			//	fmt.Printf("%d:0x%02X ", i, buff[i])
-			//}
-			//fmt.Printf("\n\n")
 			packet, err := eg4.Parse(buff)
 			if err != nil {
-				fmt.Printf("Error parsing packet: %v\n", err)
+				level.Error(logger).Log("msg", "error parsing packet", "err", err)
+				continue
 			}
 			ps, err := json.Marshal(packet)
 			if err != nil {
-				fmt.Printf("Failed to marshal packet to json: %v\n", err)
+				level.Error(logger).Log("msg", "failed to marshal packet to json", "err", err)
+				continue
 			}
 			fmt.Println(string(ps))
+			err = c.Handle(labels, time.Now(), string(ps))
+			if err != nil {
+				level.Error(logger).Log("msg", "failed to send logs to loki client", "err", err)
+			}
+			sb := strings.Builder{}
+			for i := 0; i < n; i++ {
+				sb.WriteString(fmt.Sprintf("%d:0x%02X ", i, buff[i]))
+			}
+			err = c.Handle(labels_raw, time.Now(), sb.String())
+			if err != nil {
+				level.Error(logger).Log("msg", "failed to send logs to loki client", "err", err)
+			}
 		} else {
-			fmt.Printf("Did not receive valid packet from battery, ignoring this poll.\n")
+			level.Error(logger).Log("msg", "did not receive valid packet from battery, ignoring this poll.")
 		}
-
-		time.Sleep(1000 * time.Millisecond)
-		//fmt.Printf("0x%X ", string(buff[:n]))
 	}
 
 }
